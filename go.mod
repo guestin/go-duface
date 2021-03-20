5676563b27beedf3f4da36cb9982ed7466f3599a
suzhen <suzhen@guestin.cn>
2021-03-20 14:46:09 +0800

    upgrade dependency

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/guestin/go-duface
 go 1.16
 
 require (
-	github.com/guestin/go-requests v0.0.8
+	github.com/guestin/go-requests v0.0.9
 	github.com/guestin/mob v1.0.7
 )
--- internal/opt_dump_response.go
+++ internal/opt_dump_response.go
@@ -1,29 +1,27 @@
 package internal
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/guestin/go-requests/opt"
 	"io"
 )
 
-func DumpResponse(tag string) opt.Option {
-	return func(context *opt.RequestContext) error {
-		old := context.ResponseHandler
-		context.ResponseHandler = func(statusCode int, stream io.Reader) (interface{}, error) {
-			fmt.Printf("[%s] status code=%d\n", tag, statusCode)
-			respBytes, err := io.ReadAll(stream)
-			if err != nil {
-				fmt.Printf("[%s] read response error:%s\n", tag, err)
-				return nil, err
-			}
-			fmt.Printf("[%s] response:%s\n", tag, respBytes)
-			if old != nil {
-				return old(statusCode, bytes.NewReader(respBytes))
-			}
-			return nil, nil
-		}
+func DumpResponse(tag string, skip bool) opt.Option {
+	return func(reqCtx *opt.RequestContext) error {
+		reqCtx.InstallResponseHandler(
+			func(statusCode int, stream io.Reader, previousValue interface{}) (interface{}, error) {
+				if skip {
+					return previousValue, nil
+				}
+				fmt.Printf("[%s] status code=%d\n", tag, statusCode)
+				respBytes, err := io.ReadAll(stream)
+				if err != nil {
+					fmt.Printf("[%s] read response error:%s\n", tag, err)
+					return nil, err
+				}
+				fmt.Printf("[%s] response:%s\n", tag, respBytes)
+				return nil, nil
+			}, opt.HEAD)
 		return nil
 	}
-
 }
